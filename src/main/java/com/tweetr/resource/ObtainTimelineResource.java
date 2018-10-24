6de628ab5dbfd5223aebf14f8d21c832ed3da938
kaushikpatidar <kaushik.patidar@lithium.com>
2018-10-24 15:27:04 +0530

    Using the Java 8 Streams Added a new resource endpoint to filter the timeline messages. Added try-catch statements and null/empty checks for query parameters in the endpoints

--- src/main/java/com/tweetr/resource/ObtainTimelineResource.java
+++ src/main/java/com/tweetr/resource/ObtainTimelineResource.java
@@ -1,6 +1,7 @@
 package com.tweetr.resource;
 
 import com.codahale.metrics.annotation.Timed;
+import com.tweetr.model.api.constants.APIResponse;
 import com.tweetr.model.twitter.TwitterPost;
 import com.tweetr.service.TwitterService;
 import org.slf4j.Logger;
@@ -10,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/timeline")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,7 +26,6 @@ public class ObtainTimelineResource {
 
     private Logger log = LoggerFactory.getLogger(ObtainTimelineResource.class);
 
-    //@Inject
     public ObtainTimelineResource(TwitterService twitterService) {
         this.twitterService = twitterService;
     }
@@ -35,14 +37,49 @@ public class ObtainTimelineResource {
     @GET
     @Timed
     public Response getHomeTimeline() {
-        log.info("Getting the timeline from twitter");
+        try {
+            log.info("Getting the timeline from twitter");
 
-        List<TwitterPost> timelineTwitterPost = twitterService.getTimelineTwitterPost();
-        if(timelineTwitterPost != null && timelineTwitterPost.size() > 0){
-            log.info("Successfully retrieved " + timelineTwitterPost.size() + " posts from timeline");
-            return Response.ok(timelineTwitterPost).build();
+            List<TwitterPost> timelineTwitterPost = twitterService.getTimelineTwitterPost();
+            if(timelineTwitterPost != null && timelineTwitterPost.size() > 0){
+                log.info("Successfully retrieved " + timelineTwitterPost.size() + " posts from timeline");
+                return Response.ok(timelineTwitterPost).build();
+            } else if(timelineTwitterPost != null){
+                log.info("There are no posts from timeline");
+                return Response.ok(timelineTwitterPost).build();
+            }
+        } catch (Exception e){
+            log.error("Error: " + e.getMessage(), e);
         }
+        return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
 
+    @GET
+    @Path("/filter")
+    public Response getFilteredHomeTimeline(@QueryParam("filterString") final String tweetFilter){
+        try {
+            log.info("Getting the filtered timeline from twitter");
+
+            if(tweetFilter == null || tweetFilter.length() == 0){
+                return Response.ok(APIResponse.OBTAIN_TIMELINE_NO_FILTER_PROVIDED).build();
+            }
+
+            List<TwitterPost> timelineTwitterPost = twitterService.getTimelineTwitterPost();
+            if(timelineTwitterPost != null && timelineTwitterPost.size() > 0){
+                log.info("Successfully retrieved " + timelineTwitterPost.size() + " posts from timeline");
+
+                List<TwitterPost> filteredTimelinePosts = timelineTwitterPost.stream()
+                        .filter(post -> post.getMessage().contains(tweetFilter))
+                        .collect(Collectors.toList());
+
+                return Response.ok(filteredTimelinePosts).build();
+            } else if(timelineTwitterPost != null){
+                log.info("There are no posts from timeline");
+                return Response.ok(timelineTwitterPost).build();
+            }
+        } catch (Exception e){
+            log.error("Error: " + e.getMessage(), e);
+        }
         return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
 }
